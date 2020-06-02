f1eccb89aef215fb345681ec933e999944559f25
Anh Vuong <vuonga1103@yahoo.com>
2020-06-02 15:40:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(message)
     count += 1
   end
 end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < number do
+    puts message
+    count += 1
+  end
+end
